9ab280930901c339ce81fff8cd082be7066171d2
Esmaeil MIRZAEE <esmaeil@g!>
2022-07-11 09:40:18 +0000

    Initialize application

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+function Home(): NextPage {
+  return (
+    <div className="bg-white dark:bg-stone-800 text-stone-800 dark:text-white min-h-screen">
+      <h1 className="font-bold text-4xl">A new Next project</h1>
+    </div>
+  );
+}
+
+export default Home;
